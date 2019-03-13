bc0be946e919c5085e76d50970e55ebafa2b3d91
JekaRUS <jekarus47@gmail.com>
2019-03-13 13:08:29 +0300

    Вспомогательные классы для переменных в локализации

---
+++ src/main/java/ru/jekarus/skyfortress/v3/lang/LanguageVariables.java
@@ -0,0 +1,52 @@
+package ru.jekarus.skyfortress.v3.lang;
+
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.TextTemplate;
+import org.spongepowered.api.text.format.TextColor;
+import ru.jekarus.skyfortress.v3.player.SfPlayer;
+import ru.jekarus.skyfortress.v3.team.SfTeam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LanguageVariables {
+
+    private Map<String, Text> variables = new HashMap<>();
+
+    private final SfLanguage language;
+
+    public LanguageVariables(SfLanguage language) {
+        this.language = language;
+    }
+
+    public LanguageValueVariables key(String key) {
+        return new LanguageValueVariables(this, language, key);
+    }
+
+    public LanguageValueVariables playerKey(String prefix) {
+        return this.key(prefix + ".player.name");
+    }
+
+    public LanguageValueVariables playerKey() {
+        return this.key("player.name");
+    }
+
+    public LanguageValueVariables teamKey(String prefix) {
+        return this.key(prefix + ".team");
+    }
+
+    public LanguageValueVariables teamKey() {
+        return this.key("team");
+    }
+
+    public LanguageVariables appendCustom(String key, Text value) {
+        this.variables.put(key, value);
+        return this;
+    }
+
+    public Text apply(TextTemplate template) {
+        return template.apply(this.variables).build();
+    }
+
+}
