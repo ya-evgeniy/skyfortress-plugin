4276a94eff2c353eef0fefa006a2d0ccd0b28c7b
JekaRUS <jekarus47@gmail.com>
2019-06-21 08:41:18 +0300

    cosmetic and saturation in EndGameStage

--- src/main/java/ru/jekarus/skyfortress/v3/game/EndGameStage.java
+++ src/main/java/ru/jekarus/skyfortress/v3/game/EndGameStage.java
@@ -2,52 +2,58 @@ package ru.jekarus.skyfortress.v3.game;
 
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.mutable.PotionEffectData;
+import org.spongepowered.api.effect.potion.PotionEffect;
+import org.spongepowered.api.effect.potion.PotionEffectTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.gamemode.GameModes;
 import ru.jekarus.skyfortress.v3.SkyFortressPlugin;
-import ru.jekarus.skyfortress.v3.lobby.SfLobby;
 import ru.jekarus.skyfortress.v3.player.PlayerZone;
 import ru.jekarus.skyfortress.v3.player.SfPlayer;
 import ru.jekarus.skyfortress.v3.player.SfPlayers;
+import ru.jekarus.skyfortress.v3.scoreboard.SfScoreboards;
+import ru.jekarus.skyfortress.v3.settings.LobbySettings;
 
 public class EndGameStage extends SfGameStage {
 
     private final SkyFortressPlugin plugin;
 
-    public EndGameStage(SkyFortressPlugin plugin)
-    {
+    public EndGameStage(SkyFortressPlugin plugin) {
         this.plugin = plugin;
     }
 
     @Override
-    public void enable()
-    {
-        this.plugin.getScoreboards().clearSideBar();
-        SfLobby lobby = this.plugin.getLobby();
+    public void enable() {
+        this.plugin.getScoreboards().setSideBar(SfScoreboards.Types.POST_GAME);
+        this.plugin.getScoreboards().updatePlaces();
+        final LobbySettings lobbySettings = plugin.getSettings().getLobby();
         SfPlayers players = SfPlayers.getInstance();
-        for (Player player : Sponge.getServer().getOnlinePlayers())
-        {
+        for (Player player : Sponge.getServer().getOnlinePlayers()) {
             player.setLocationAndRotation(
-                    lobby.getSettings().center.getLocation(),
-                    lobby.getSettings().center.getRotation()
+                    lobbySettings.getCenter().getLocation(),
+                    lobbySettings.getCenter().getRotation()
             );
             player.offer(Keys.GAME_MODE, GameModes.ADVENTURE);
             SfPlayer sfPlayer = players.getOrCreatePlayer(player);
             this.plugin.getTeamContainer().getNoneTeam().addPlayer(this.plugin, sfPlayer);
             sfPlayer.setZone(PlayerZone.LOBBY);
             player.getInventory().clear();
+            player.getOrCreate(PotionEffectData.class).ifPresent(effects -> {
+                effects.addElement(
+                        PotionEffect.builder().potionType(PotionEffectTypes.SATURATION).duration(1_000_000).amplifier(255).particles(false).build()
+                );
+                player.offer(effects);
+            });
         }
     }
 
     @Override
-    public void disable()
-    {
+    public void disable() {
 
     }
 
     @Override
-    public SfGameStageType getType()
-    {
+    public SfGameStageType getType() {
         return SfGameStageType.END_GAME;
     }
 
