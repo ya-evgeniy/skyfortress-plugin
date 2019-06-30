e88afeca153ca31d789257ed453e397269e0afab
JekaRUS <jekarus47@gmail.com>
2019-06-30 14:30:59 +0300

    fix friendly fire

--- src/main/java/ru/jekarus/skyfortress/v3/listener/FriendFireListener.java
+++ src/main/java/ru/jekarus/skyfortress/v3/listener/FriendFireListener.java
@@ -1,13 +1,15 @@
 package ru.jekarus.skyfortress.v3.listener;
 
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.projectile.Projectile;
+import org.spongepowered.api.entity.projectile.source.ProjectileSource;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.cause.entity.damage.source.EntityDamageSource;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.filter.cause.First;
-import org.spongepowered.api.event.filter.cause.Root;
 import ru.jekarus.skyfortress.v3.SkyFortressPlugin;
 import ru.jekarus.skyfortress.v3.player.PlayerData;
 import ru.jekarus.skyfortress.v3.player.PlayersDataContainer;
@@ -33,16 +35,29 @@ public class FriendFireListener {
     }
 
     @Listener
-    public void on(DamageEntityEvent event, @Root EntityDamageSource source) {
-        if (!source.getSource().getType().equals(EntityTypes.PLAYER)) {
-            return;
-        }
+    public void on(DamageEntityEvent event, @First EntityDamageSource source) {
         if (!event.getTargetEntity().getType().equals(EntityTypes.PLAYER)) {
             return;
         }
 
-        final Player damager = (Player) source.getSource();
-        final Player target = (Player) event.getTargetEntity();
+        Player target = (Player) event.getTargetEntity();
+        Player damager = null;
+
+        Entity killerEntity = source.getSource();
+        if (killerEntity.getType().equals(EntityTypes.PLAYER)) {
+            damager = (Player) killerEntity;
+        }
+        else if (killerEntity instanceof Projectile) {
+            final Projectile projectile = (Projectile) killerEntity;
+            final ProjectileSource shooter = projectile.getShooter();
+            if (shooter instanceof Player) {
+                damager = (Player) shooter;
+            }
+        }
+
+        if (damager == null) {
+            return;
+        }
 
         final Optional<PlayerData> optionalDamagerData = playersData.get(damager);
         if (!optionalDamagerData.isPresent()) return;
